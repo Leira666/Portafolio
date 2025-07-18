7c9adc50f0a635650d24e172f5afd3d7067289e0
Arielweb <91092440+Leira666@users.noreply.github.com>
2025-07-18 01:40:37 -0600

    login

---
+++ index.js
@@ -0,0 +1,8 @@
+const nombre = localStorage.getItem('nombreUsuario');
+if (nombre){
+    document.getElementById.getItem('mensajeBienvenida').textContent = 
+    'Hi, ${nombre}! Welcome to my portafolio'= 'index.html';
+}else {
+    document.getElementById('mensajedefalla').textContent = 
+    'Hey! put your name';
+}
